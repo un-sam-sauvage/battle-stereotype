7ab4a3aa919a2bba6e938399b095c8b909bd0faa
Timothé <rondeautimothe@gmail.com>
2019-12-03 14:59:15 +0100

    PauseMenuV2F1

--- sam/battle_stereotype/Assets/ScriptTimothéR/PauseMenu.cs
+++ sam/battle_stereotype/Assets/ScriptTimothéR/PauseMenu.cs
@@ -15,7 +15,7 @@ public class PauseMenu : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {// Sert à activer le tactile pour ensuite lancer le MenuPause
         if (Input.touchCount < 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -33,27 +33,27 @@ public class PauseMenu : MonoBehaviour
 
     }
     public void Resume()
-    {
+    {// Sert à reprendre le jeu
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
   public void Pause()
-    {
+    {// Sert à freeze le jeu et atcive le MenuPause
 
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
     public void LoadMenu()
-    {
+    {//Sert à Load le MenuStart et remet le temps de jeu à 1f
 
         Time.timeScale = 1f;
-
+        SceneManager.LoadScene("menu_start");
 
     }
     public void QuitGame()
-    {
+    {//Quit l'appli
 
         Application.Quit();
 
