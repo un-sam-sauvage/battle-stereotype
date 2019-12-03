802d3314239873cddda5531dfd1f4dd11520a4fb
Kilian <haddesgaming@gmail.com>
2019-12-03 13:38:23 +0100

    Création script avec classe combat

---
+++ sam/battle_stereotype/Assets/Battle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour
+{
+    public Combat[] combats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+[System.Serializable]
+public class Combat
+{
+    public string stereotype;
+    public string rep1, rep2, rep3, rep4;
+    public Sprite monsterSprite;
+    public string answer;
+}
