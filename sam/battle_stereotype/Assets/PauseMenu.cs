d8d83cf605e415a65dd71977232b7680bd0b6678
Timothé <rondeautimothe@gmail.com>
2019-12-03 13:11:06 +0100

    PauseMenuV1.2

--- sam/battle_stereotype/Assets/PauseMenu.cs
+++ sam/battle_stereotype/Assets/PauseMenu.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,36 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount < 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (GameIsPaused)
+            {
+                Resume();
+
+            } else
+            {
+
+                Pause();
+            }
+
+        }
+        void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+        }
+        void Pause()
+        {
+
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+        }
+
+
+
+
     }
 }
