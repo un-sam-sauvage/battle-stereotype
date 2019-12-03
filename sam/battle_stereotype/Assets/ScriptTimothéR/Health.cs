31918911216456bd3ce2fd7c3269c092b90259cc
Timothé <rondeautimothe@gmail.com>
2019-12-03 16:20:44 +0100

    HealthSystemV1

---
+++ sam/battle_stereotype/Assets/ScriptTimothéR/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullheart;
+    public Sprite emptyHeart;
+    // Start is called before the first frame update
+     void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            } else
+            {
+                hearts[i].enabled = false;
+            }
+
+        }
+        {
+
+
+        }
+
+
+
+    }
+}
