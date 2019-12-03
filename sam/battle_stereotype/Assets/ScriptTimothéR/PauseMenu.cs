fe29d5836b924f70f7e3d368beef01a79bd834f3
Timothé <rondeautimothe@gmail.com>
2019-12-03 14:39:34 +0100

    PauseMenuV1.6

--- sam/battle_stereotype/Assets/ScriptTimothéR/PauseMenu.cs
+++ sam/battle_stereotype/Assets/ScriptTimothéR/PauseMenu.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
@@ -44,6 +44,19 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+
+        Time.timeScale = 1f;
+
+
+    }
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
     }
 }
 
