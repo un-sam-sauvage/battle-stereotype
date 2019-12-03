ba95e59b0b89c8f48e4add9a3ddbe157a06bf113
Sam <samuel.brb19@gmail.com>
2019-12-03 18:28:41 +0100

    le retour

---
+++ sam/battle_stereotype/Assets/script_menu_start/changement_scene_menu_start.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changement_scene_menu_start : MonoBehaviour
+{
+    public string sceneNameToLoad ;
+    bool allowchange=false;
+    bool startCountdown=false;
+    public GameObject imageFade;
+    float countdown = 1f;
+
+    public void ChangementScene()
+    {
+        imageFade.SetActive(true);
+        startCountdown = true;          
+    }
+    void Update()
+    {
+        if (startCountdown == true)
+        {
+            countdown -= Time.deltaTime;
+            Debug.Log(countdown);
+        }
+        if (countdown <= 0)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+
+    }
+}
