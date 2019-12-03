a7ba1ba66f012aedcc2e7b50187519dd02c218ef
Kilian <haddesgaming@gmail.com>
2019-12-03 16:01:31 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ sam/battle_stereotype/Assets/Sound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Sound : MonoBehaviour
+{
+    public string nameSound;
+
+    public AudioClip clip ;
+
+    [Range(0f, 1f)] 
+    public float volume ;
+
+    public bool loop ;
+
+    [HideInInspector]
+    public AudioSource source ;
+}
