78031a3da3da5b4d6f685767f76337bf5ca44be3
Kilian <haddesgaming@gmail.com>
2019-12-03 14:31:25 +0100

    Script Combat v1

--- sam/battle_stereotype/Assets/Battle.cs
+++ sam/battle_stereotype/Assets/Battle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Battle : MonoBehaviour
 {
@@ -8,6 +9,25 @@ public class Battle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Combat combats1 = combats[1];
+        Debug.Log(combats1);
+        if(combats1 == combats[0])
+        {
+            combats1.rep1.text = "Test";
+            combats1.rep2.text = "Test";
+            combats1.rep3.text = "Test";
+            combats1.rep4.text = "Test";
+        }
+        else if(combats1 == combats[1])
+        {
+            combats1.rep1.text = "Là c'est le deuxième";
+            combats1.rep2.text = "Oui";
+            combats1.rep3.text = "Ah";
+            combats1.rep4.text = "Ok";
+        }
+        
+
+        
         
     }
 
@@ -22,8 +42,9 @@ public class Battle : MonoBehaviour
 [System.Serializable]
 public class Combat
 {
+    public string name;
     public string stereotype;
-    public string rep1, rep2, rep3, rep4;
+    public Text rep1, rep2, rep3, rep4;
     public Sprite monsterSprite;
     public string answer;
 }
