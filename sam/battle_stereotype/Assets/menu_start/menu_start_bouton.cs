c07b6352fa8d758a6641643a607d42891909ad88
sam <samuel.brb19@gmail.com>
2019-12-03 14:55:51 +0100

    menu start

---
+++ sam/battle_stereotype/Assets/menu_start/menu_start_bouton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_start_bouton : MonoBehaviour
+{
+    public GameObject imageFade;
+    float countdown=1f;
+    bool startCountdonwn=false;
+    public string sceneNameToLoad;
+
+    public void ChangeScene()
+    {
+        //active l'image ce qui déclenche l'animation et le compte à rebours.
+        imageFade.SetActive(true);
+        startCountdonwn = true;
+    }
+    void Update()
+    {
+        //attend une seconde que l'animation se joue puis change de scène.
+        if (startCountdonwn == true)
+        {
+            countdown -= Time.deltaTime;
+        }
+        if (countdown <= 0)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
