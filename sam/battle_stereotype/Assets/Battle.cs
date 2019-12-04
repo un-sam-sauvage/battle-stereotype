1d2194da0aa689ef5091ec9ad2c7f5044b79fa65
Kilian <haddesgaming@gmail.com>
2019-12-04 12:02:39 +0100

    Combat complet pour un duel

--- sam/battle_stereotype/Assets/Battle.cs
+++ sam/battle_stereotype/Assets/Battle.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class Battle : MonoBehaviour
 {
     public Combat[] combats;
-    int x;
     Combat combats1;
+    int x;
+    
 
     public Button button1, button2, button3;
     // Start is called before the first frame update
     void Start()
     {
-        x = Random.Range(0, 5);
-        combats1 = combats[x];
+        x = Random.Range(0, combats.Length);
+        combats1 = combats[0];
+
+        
         
         Debug.Log(x);
 
@@ -28,6 +31,43 @@ public class Battle : MonoBehaviour
     }
 
     public void PlayerTurn()
+    {
+        Button b1 = button1.GetComponent<Button>();
+        Button b2 = button2.GetComponent<Button>();
+        Button b3 = button3.GetComponent<Button>();
+
+        b1.onClick.AddListener(PrintAnswer1);
+        b2.onClick.AddListener(PrintAnswer2);
+        b3.onClick.AddListener(PrintAnswer3);
+
+
+        if (combats1.answer.text == combats1.goodAnswer.text)
+        {
+            Debug.Log("You win !");
+        }
+        else
+        {
+            Debug.Log("You loose...");
+        }
+    }
+
+    public void PrintAnswer1()
+    {
+        combats1.answer.text = combats1.rep1.text;
+    }
+
+    public void PrintAnswer2()
+    {
+        combats1.answer.text = combats1.rep2.text;
+    }
+
+    public void PrintAnswer3()
+    {
+        combats1.answer.text = combats1.rep3.text;
+    }
+
+
+    void Update()
     {
         
     }
