9c5e6fa8f40c0eed1877f99c6b81daf35acaf015
Sam <samuel.brb19@gmail.com>
2019-12-04 09:35:36 +0100

    réparation main scene

---
+++ sam/battle_stereotype/Assets/MainScene/TouchDrag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDrag : TouchSprite
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TouchInput(GetComponent<BoxCollider2D>());
+    }
+    void onFisrtTouch()
+    {
+        Vector2 pos;
+        pos = new Vector2(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x, Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).y);
+    }
+}
