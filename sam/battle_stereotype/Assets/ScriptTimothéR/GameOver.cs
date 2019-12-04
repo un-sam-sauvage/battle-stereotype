8251b40b0321a3d00422b887fb3e754c1a9c8715
Timothé <rondeautimothe@gmail.com>
2019-12-04 11:32:12 +0100

    GAMEOVER panelV1

---
+++ sam/battle_stereotype/Assets/ScriptTimothéR/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void LoadMenu()
+    {//Sert à Load le MenuStart 
+
+
+        SceneManager.LoadScene("menu_start");
+    }
+}
