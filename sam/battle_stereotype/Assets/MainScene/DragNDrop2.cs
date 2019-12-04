042480d90a0f95c00bfaea75a02b6204b820dfc6
Sam <samuel.brb19@gmail.com>
2019-12-04 10:47:23 +0100

    Drag N Drop fonctionne

---
+++ sam/battle_stereotype/Assets/MainScene/DragNDrop2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragNDrop2 : MonoBehaviour , IDragHandler, IEndDragHandler
+{
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Touch touch = Input.GetTouch(0);
+        //Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+        Vector2 touchPos = touch.position;
+        transform.position = touchPos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = Vector2.zero;
+    }
+}
+ 
