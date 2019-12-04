52bfc0fd315ff1795e3606042a4049533fada833
Sam <samuel.brb19@gmail.com>
2019-12-04 13:02:24 +0100

    drag n drop fini

--- sam/battle_stereotype/Assets/MainScene/DragNDrop2.cs
+++ sam/battle_stereotype/Assets/MainScene/DragNDrop2.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragNDrop2 : MonoBehaviour , IDragHandler, IEndDragHandler
+public class DragNDrop2 : MonoBehaviour , IDragHandler, IEndDragHandler , IBeginDragHandler
 {
-    public GameObject boss;
-    public GameObject bouton;
 
-    // lorsque le joueur drag un objet, l'objet prend les coordonnées de l'endroit où le joueur touche l'écran 
+    Vector2 startPosition;
+    public static GameObject objectBeingDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+        objectBeingDrag = gameObject;
+    }
+
+    // lorsque le joueur drag un objet, l'objet prend les coordonnées de l'endroit où le joueur touche l'écran et l'objet prend ces coordonnées.
     public void OnDrag(PointerEventData eventData)
     {
         Touch touch = Input.GetTouch(0);
         Vector2 touchPos = touch.position;
         transform.position = touchPos;
-        RectTransform boss = transform as RectTransform;
-        if (!RectTransformUtility.RectangleContainsScreenPoint(boss,touchPos))
-        {
-            bouton.SetActive(true);
-        }
         
     }
-
+    // si l'on relâche l'écran, l'objet revient à sa position de départ.
     public void OnEndDrag(PointerEventData eventData)
     {
-        transform.localPosition = Vector2.zero;
+        objectBeingDrag = null;
+        transform.position = startPosition;
     }
 }
  
---
+++ sam/battle_stereotype/Assets/MainScene/bossDetectCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class bossDetectCollision : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 1)
+            {
+                return transform.GetChild(1).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragNDrop2.objectBeingDrag.SetActive(false);
+        }
+    }
+}
