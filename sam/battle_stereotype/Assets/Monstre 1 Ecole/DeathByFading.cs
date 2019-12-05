ce1c197d4317d9db38a5ac232d50638fb769456b
Kilian <haddesgaming@gmail.com>
2019-12-05 15:41:16 +0100

    2 script pour la mort des monstres

---
+++ sam/battle_stereotype/Assets/Monstre 1 Ecole/DeathByFading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathByFading : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    float a = 1f;
+    float x = 0.02f;
+
+    public void DeathByFade()
+    {
+        a -= x; //a perd x à chaque frame
+        spriteRenderer = GetComponent<SpriteRenderer>();    //on récupère les composants du SpriteRenderer
+        spriteRenderer.color = new Color(1, 1, 1, a);   //on actualise l'opacité de la couleur du sprite
+
+        if (a <= 0)
+        {
+            Destroy(gameObject);    //Quand a atteint 0 ou moins, on détruit l'objet
+        }
+    }
+}
---
+++ sam/battle_stereotype/Assets/Monstre 1 Ecole/DeathByScaling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathByScaling : MonoBehaviour
+{
+    float x = 0.05f;
+    float y = 0.05f;
+    float z = 0.05f;
+
+    public void DeathByScale()
+    {
+        transform.localScale -= new Vector3(x, y, z);   //On réduit la scale de l'objet de 0.05 toutes les frames
+
+        if (transform.localScale.x <= 0 && transform.localScale.y <= 0 && transform.localScale.z <= 0)
+        {
+            Destroy(gameObject);    //Quand chaque valeur du scale atteint 0 ou moins, on détuit l'objet
+        }
+    }
+}
