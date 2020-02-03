a483c9fdcbd13fa49fe05851aba84290236ca0bb
Sam <samuel.brb19@gmail.com>
2020-02-03 15:31:24 +0100

    panel a implémenter pour annonce niveau

---
+++ sam/battle_stereotype/Assets/MainScene/AnimPanelNiveau.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using DG.Tweening;
+using Google.GData.Documents;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimPanelNiveau : MonoBehaviour
+{
+    private Image img;
+    void Start()
+    {
+        
+        img = GetComponent<Image>();
+        
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(transform.DOScale(new Vector2(1.5f, 1.5f), 5));
+        mySequence.Join(img.DOFade(0,5));
+        mySequence.Join(GetComponentInChildren<TextMeshProUGUI>().DOFade(0, 5));
+    }
+
+}
