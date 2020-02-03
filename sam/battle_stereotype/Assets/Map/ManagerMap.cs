b46560ce1633951f509b3eeec6d9a1c2c2a80c1a
Sam <samuel.brb19@gmail.com>
2020-02-03 14:47:25 +0100

    modif map

---
+++ sam/battle_stereotype/Assets/Map/ManagerMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerMap : MonoBehaviour
+{    
+    public static int nbLvlAllow=1;
+    public GameObject[] button;
+
+    private void Start()
+    {
+        for (int i = 0; i < nbLvlAllow; i++)
+        {
+            button[i].SetActive(true);
+        }
+    }
+}
---
+++ sam/battle_stereotype/Assets/Map/moveCrows.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class moveCrows : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = touch.position;
+            transform.DOMove(touchPos, 2);
+        }
+    }
+}
