4032aa73e3fdba5f2c9863ebfbff088b7a05b1ee
Timothé <rondeautimothe@gmail.com>
2020-02-10 15:04:14 +0100

    push_particules

---
+++ sam/battle_stereotype/Assets/ScriptTimothéR/particleActivate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleActivate : MonoBehaviour
+{
+    public ParticleSystem particule;
+
+    public bool particuleActivate = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particule = GetComponent<ParticleSystem>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {// Permet de réactiver les particules + les désactivés
+        if (particuleActivate)
+        {
+            particule.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+        else if (particule.isStopped && particuleActivate == false)
+        {
+            particule.Play(gameObject);
+        }
+        
+    }
+}
