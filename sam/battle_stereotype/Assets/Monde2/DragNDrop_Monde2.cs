95b7a4611e16f87344ff702df39a9e829c2a14f3
Kilian <haddesgaming@gmail.com>
2020-02-18 14:11:21 +0100

    Avancement monde 2 (ancrage, récup données sheets, etc)

---
+++ sam/battle_stereotype/Assets/Monde2/DragNDrop_Monde2.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragNDrop_Monde2 : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+
+    Vector2 startPosition;
+    public static GameObject objectBeingDrag;
+
+    public GameObject bulleFoule;
+    public TMP_Text answerDragged;
+    public static bool enabledTransfer = false;
+
+    float countdown = 5f;
+
+    void Start()
+    {
+        Input.multiTouchEnabled = false;    //On limite le fait qu'on ait plusieurs touchés sur l'écran
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        enabledTransfer = false;
+        startPosition = transform.position;
+        objectBeingDrag = gameObject;
+    }
+
+    // lorsque le joueur drag un objet, l'objet prend les coordonnées de l'endroit où le joueur touche l'écran et l'objet prend ces coordonnées.
+    public void OnDrag(PointerEventData eventData)
+    {
+        Touch touch = Input.GetTouch(0);
+        Vector2 touchPos = touch.position;
+        transform.position = touchPos;
+
+    }
+    // si l'on relâche l'écran, l'objet revient à sa position de départ. Il envoie aussi le contenu du texte au programme de bataille.
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        objectBeingDrag = null;
+        transform.position = startPosition;
+        if (enabledTransfer == true)
+        {
+            Battle_Monde2 battle = FindObjectOfType<Battle_Monde2>();
+            battle.VerifAnswer(answerDragged);
+        }
+
+
+    }
+    public void Update()
+    {
+        if (enabledTransfer == true)    //Après 5 secondes, on désactive la bulle de texte de la foule
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0)
+            {
+                bulleFoule.SetActive(false);
+                countdown = 5f;
+            }
+        }
+    }
+
+
+
+}
